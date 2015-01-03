c1b79e071f277ff3aaf2b6832728c44857987a61
andyhuang91 <andyh@mit.edu>
2015-01-03 13:20:02 -0800

    Add ball and collision checking
    Adding a ball entity and collision checking between the ball and both
    the walls and the paddle.

--- game.js
+++ game.js
@@ -14,7 +14,8 @@ function Game() {
   this.ctx = canvas.getContext('2d');
   this.ctx.fillStyle = 'white';
 
-  this.player1 = new Entity(300,200, 60, 15);
+  this.player1 = new Entity(100, 200, 60, 15);
+  this.ball = new Ball(300, 200, 10, new Vector(1,0));
 
   this.inputEngine = new InputEngine(canvas);
 
@@ -32,7 +33,12 @@ Game.prototype.updateState = function() {
   
   newVelocity.normalize();
   this.player1.velocity = newVelocity;
+
+  this.checkWallCollisions();
+  this.checkPaddleCollisions();
+
   this.player1.move();
+  this.ball.move();
   this.render();
 }
 
@@ -43,13 +49,40 @@ Game.prototype.drawEntity = function(entity) {
 Game.prototype.render = function() {
   this.ctx.clearRect(0, 0, this.width, this.height);
   this.drawEntity(this.player1);
+  this.drawEntity(this.ball);
 }
 
-function Entity(xPos, yPos, height, width, ctx) {
+Game.prototype.checkWallCollisions = function() {
+  var ballPos = this.ball.position,
+      ballVel = this.ball.velocity,
+      player1Pos = this.player1.position;
+
+  if (ballPos.x < 0) {
+    ballVel.x = Math.abs(ballVel.x);
+  } else if (ballPos.x > this.width) {
+    ballVel.x = -Math.abs(ballVel.x);
+  }
+
+  if (ballPos.y < 0) {
+    ballVel.y = Math.abs(ballVel.x);
+  } else if (ballPos.y > this.height) {
+    ballVel.y = -Math.abs(ballVel.y);
+  }
+}
+
+Game.prototype.checkPaddleCollisions = function() {
+  var paddleCollision = this.ball.checkCollision(this.player1);
+
+  if (paddleCollision) {
+    var newXDirection = (this.ball.position.x > this.player1.position.x) ? 1 : -1;
+    this.ball.velocity.x =  newXDirection * Math.abs(this.ball.velocity.x);
+  }
+}
+
+function Entity(xPos, yPos, height, width) {
   this.position = new Vector(xPos, yPos);
   this.height = height;
   this.width = width;
-  this.ctx = ctx;
   this.velocity = new Vector(0, 0);
   this.maxSpeed = 10;
 }
@@ -65,6 +98,37 @@ Entity.prototype.draw = function(ctx) {
   var pos = this.position;
   ctx.fillRect(pos.x - this.width/2, pos.y - this.height/2, this.width, this.height);
 }
+
+Entity.prototype.getBoundingBox = function() {
+  var halfHeight = this.height/2,
+      halfWidth = this.width/2;
+
+  var boundingBox = {
+    top: this.position.y - halfHeight,
+    bottom: this.position.y + halfHeight,
+    left: this.position.x - halfWidth,
+    right: this.position.x + halfWidth
+  };
+
+  return boundingBox;
+}
+
+Entity.prototype.checkCollision = function(entity) {
+  var myBB = this.getBoundingBox(),
+      otherBB = entity.getBoundingBox();
+
+  return !(myBB.left > otherBB.right ||
+          myBB.right < otherBB.left ||
+          myBB.top > otherBB.bottom ||
+          myBB.bottom < otherBB.top);
+}
+
+function Ball(xPos, yPos, radius, initialVelocity) {
+  Entity.call(this, xPos, yPos, radius, radius);
+  this.velocity = initialVelocity;
+}
+
+Ball.prototype = Object.create(Entity.prototype);
 
 function Vector(x, y) {
   this.x = x;
